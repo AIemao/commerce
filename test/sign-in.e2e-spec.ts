9d7b654c8d2fc74fda87fa85022232f8c3f2145e
Celio Junior <celioju@hotmail.com>
2025-06-12 12:04:29 -0300

    Setup

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('testemockOK@email.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Enviamos um link de acesso')
+
+  expect(toast).toBeVisible({ timeout: 10000 })
+  await page.waitForTimeout(2000) // Wait for the toast to appear
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Erro ao realizar login. Tente')
+
+  expect(toast).toBeVisible({ timeout: 10000 })
+  await page.waitForTimeout(2000) // Wait for the toast to appear
+})
+
+test('navigate to new store page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
